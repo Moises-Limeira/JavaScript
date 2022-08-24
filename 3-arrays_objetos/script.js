c12ef9e12ca696a270c03a2408415979fc5924ab
Moises-Limeira <limeir17@yahoo.com>
2022-08-24 03:01:32 -0300

    Arrays de objetos

---
+++ 3-arrays_objetos/script.js
@@ -0,0 +1,30 @@
+// var list = ['Sergipe', 'São Paulo', 'Rio de Janeiro']
+// console.log(list)
+
+
+var estado1 = {
+  name: 'Sergipe',
+  municipios: 75
+}
+var estado2 = {
+  name: 'São Paulo',
+  municipios: 645
+}
+var estado3 = {
+  name: 'Rio de Janeiro',
+  municipios: 92
+}
+
+var list2 = [estado1, estado2, estado3]
+
+// console.log(
+//   list2[0].name
+//   )
+
+for (var estado of list2){
+  console.log(estado)
+}
+
+
+
+
